0a0271deff741cd99d34baab7dba277112340e4d
C1mone <c1mone.tsai@gmail.com>
2016-11-27 16:50:27 +0800

    Add start and stop command

--- bot.js
+++ bot.js
@@ -17,13 +17,68 @@ bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
   });
 });
 
-function replyWithError(userId, err) {
-    logger.warn('user: %s, message: %s', userId, err.message);
+var chat = {};
 
-    bot.sendMessage(userId, 'QQ Something wrong! ' + err, {
+bot.onText(/^\/start$/, function (msg){
+    var username = msg.from.username;
+    var chatId = msg.chat.id;
+    var chatType = msg.chat.type;
+
+    if(isGroupChatType(chatType) && isAdmin(username)){
+        logger.info("user %s with chat type %s and chat id %s", username, chatId, chatType);
+        var response = ["Hello " + username + "!"];
+        response.push("Now start auto-drop game in this chat :)")
+
+        bot.sendMessage(chatId, response.join('\n'),{
+            'parse_mode': 'Markdown',
+            'selective': 2
+        });
+    }
+
+})
+
+bot.onText(/^\/stop$/, function (msg){
+    var username = msg.from.username;
+    var chatId = msg.chat.id;
+    var chatType = msg.chat.type;
+
+    if(isGroupChatType(chatType) && isAdmin(username)){
+        var response = ["Hello " + username + "!"];
+        response.push("Now stop auto-drop game in this chat :)")
+
+        bot.sendMessage(chatId, response.join('\n'),{
+            'parse_mode': 'Markdown',
+            'selective': 2
+        });
+    }
+
+});
+
+function replyWithError(userName, chatId, err) {
+    logger.warn('user: %s, message: %s', userName, err.message);
+
+    bot.sendMessage(chatId, 'QQ Something wrong! ' + err, {
         'parse_mode': 'Markdown',
         'reply_markup': {
             'hide_keyboard': true
         }
     })
+}
+
+function isAdmin(username) {
+    if(config.bot.adminUserName === username)
+        return true;
+    else
+        return false;
+}
+
+/*
+ * Restrict chat type to group
+ */
+
+function isGroupChatType(chatType) {
+    if(chatType === "group")
+        return true;
+    else
+        return false;
 }
